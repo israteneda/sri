57c78e232a80bd08e0ada4c37c61c68a68ab03a0
Israel Teneda <israteneda@gmail.com>
2022-08-19 21:22:02 -0500

    Add more context for #1

---
+++ tests/invoice.test.ts
@@ -0,0 +1,4 @@
+// Los contribuyentes que generen sus comprobantes de venta, retención y
+// documentos complementarios firmados electrónicamente en el ambiente de
+// pruebas, pueden utilizar en el campo de la razón social del receptor, destinatario y
+// agente retenido la denominación PRUEBAS SERVICIO DE RENTAS INTERNAS.
